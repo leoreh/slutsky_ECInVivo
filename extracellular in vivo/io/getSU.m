524bc387a1250273e3dbde5c738cc982eb904bf7
Leore Heim <45538676+leoreh@users.noreply.github.com>
2018-12-23 20:29:39 +0200

    getSU debug

--- extracellular in vivo/io/getSU.m
+++ extracellular in vivo/io/getSU.m
@@ -1,17 +1,16 @@
-function SU = getSU(basepath, spikes)
+function SU = getSU(basepath, spikes, saveVar)
 
 % receives spikes struct and returns same struct but only SU
 %
 % INPUT:
-%   basepath    path to recording folder {pwd}.
-%   spikes      struct of spikes (see getSpikes)
+%   basepath            path to recording folder {pwd}.
+%   spikes              struct of spikes (see getSpikes)
 %
 % CALLS:
 %   getSpikes (optional)
 %
-% TO DO LIST:
-%
-% 18 dec 18 LH and RA.
+% 18 dec 18 LH and RA.  Updates:
+% 23 dec 18 LH          saveVar
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % arguments
@@ -24,6 +23,9 @@ if nargs < 2 || isempty(spikes)
     warning('spikes will be loaded from %s', basepath)
     spikes = getSpikes('basepath', basepath);
 end
+if nargs < 3 || isempty(saveVar)
+    saveVar = true;
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Initialize
@@ -31,8 +33,8 @@ end
 fieldSpikes = fieldnames(spikes);
 nclu = length(spikes.UID);
 c = cell(length(fieldSpikes), 1);
-spikesSU = cell2struct(c, fieldSpikes);
-spikesSU = spikes;
+SU = cell2struct(c, fieldSpikes);
+SU = spikes;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Go over fields
@@ -41,20 +43,28 @@ k = 1;
 for i = 1 : length(fieldSpikes)
     f = fieldSpikes{i};
     if length(spikes.(f)) <= 1 || isa(spikes.(f), 'char')
-        spikesSU.(f) = spikes.(f);
+        SU.(f) = spikes.(f);
         continue
     end
     for j = 1 : nclu
         if spikes.su(j) == 0
             if isa(spikes.(f), 'double')
-                spikesSU.(f)(j) = -999;
+                SU.(f)(j) = -999;
             elseif isa(spikes.(f), 'cell')
-                spikesSU.s(f){j} = -999;
+                SU.s(f){j} = -999;
             end
             k = k + 1;
         end
     end
-    spikesSU.(f)(spikesSU.(f) == -999) = [];
+    SU.(f)(SU.(f) == -999) = [];
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% save fet
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if saveVar
+    [~, filename, ~] = fileparts(basepath);
+    save([filename, '.SU'], 'SU')
 end
 
 end
