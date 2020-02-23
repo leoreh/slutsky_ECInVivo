819c056d9fd06be950e7fc3f8285ddb0689d4c2a
Leore Heim <45538676+leoreh@users.noreply.github.com>
2020-02-23 20:09:03 +0200

    spec

--- extracellular in vivo/lfp/specBand.m
+++ extracellular in vivo/lfp/specBand.m
@@ -1,16 +1,17 @@
-function [zband, tband] = specBand(varargin)
+function [zband, t] = specBand(varargin)
 
-% calculates normalized z score of power spectra in specified band. if band
-% is empty then simply calculates and plots the broadband spectrogram. can
-% also return broadband if band not specified.
+% calculates power spectra in specified band. can be normalized to power in
+% broadband (0-100 Hz). if band is empty then simply calculates and plots
+% the broadband spectrogram.
 % 
 % INPUT
 %   sig         signal for detection
 %   fs          sampling frequency
 %   band        vector. e.g. {[1 4]} for delta 
 %   binsize     scalar {10}. in [samples]
-%   smf         smooth factor {winsize}. empty means no smoothing
-%   znorm       logical. normalize z-score (1) or not {0}
+%   smf         smooth factor {winsize}. empty means no smoothing. zero
+%               means smf = binsize
+%   normband    logical. normalize to broadband (1) or not {0}
 %   graphics    logical. plot figure {1}
 %
 % OUTPUT
@@ -22,65 +23,81 @@ function [zband, tband] = specBand(varargin)
 % CALLS
 %       spectrogram     
 %
-% 13 jan 20 LH.
+% 13 jan 20 LH.     updates:
+% 20 feb 20 LH      normalize to broadband
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % arguments
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-p = inputParser;
-addParameter(p, 'sig', [], @isnumeric)
-addParameter(p, 'fs', 1250, @isnumeric)
-addParameter(p, 'band', [1 4], @isnumeric)
-addParameter(p, 'binsize', 10, @isnumeric)
-addParameter(p, 'smf', 0, @isnumeric)
-addParameter(p, 'znorm', true, @islogical)
-addParameter(p, 'graphics', false, @islogical)
+pband = inputParser;
+addParameter(pband, 'sig', [], @isnumeric)
+addParameter(pband, 'fs', 1250, @isnumeric)
+addParameter(pband, 'band', [1 4], @isnumeric)
+addParameter(pband, 'binsize', 10, @isnumeric)
+addParameter(pband, 'smf', 0, @isnumeric)
+addParameter(pband, 'normband', true, @islogical)
+addParameter(pband, 'graphics', false, @islogical)
 
-parse(p, varargin{:})
-sig = p.Results.sig;
-fs = p.Results.fs;
-band = p.Results.band;
-binsize = p.Results.binsize;
-smf = p.Results.smf;
-znorm = p.Results.znorm;
-graphics = p.Results.graphics;
+parse(pband, varargin{:})
+sig = pband.Results.sig;
+fs = pband.Results.fs;
+band = pband.Results.band;
+binsize = pband.Results.binsize;
+smf = pband.Results.smf;
+normband = pband.Results.normband;
+graphics = pband.Results.graphics;
 
 % params
-if isempty(band)
-    freq = logspace(0, 2, 100);
-else
-    freq = linspace(band(1), band(2), 40);
-end
+freq = logspace(0, 2, 100);
+
 if smf == 0
     smf = binsize;
 end
+
+% initialize output
 zband = [];
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % calc
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-win = hann(2 ^ nextpow2(binsize));
-
 % instead of 10 s window w/ 1 s overlap, 0 overlap and smooth with 10
-% points. this is so that bs and p are congruent
-[~, f, tband, pband] = spectrogram(sig, win, 0, freq, fs, 'yaxis', 'psd');
+% points. this is so that p is congruent with bsr / iis rate
+win = hann(2 ^ nextpow2(binsize));
+[pband, f, t, ~] = spectrogram(sig, win, 0, freq, fs, 'yaxis', 'psd');
 
 % psd
 pband = 10 * log10(abs(pband));
 
-% smooth and z-score
-if ~isempty(band)
-    zband = sum(pband, 1);
-    zband = movmean(zband, smf);
-    if znorm
-        zband = bz_NormToRange(zscore(zband), [0 1]);
-    end
+% normalize to broadband
+    broad = sum(pband, 1);
+    [~, idx(1)] = min(abs(f - band(1)));
+    [~, idx(2)] = min(abs(f - band(2)));
+    narrow = sum(pband(idx(1) : idx(2), :), 1);
+if normband
+    zband = bz_NormToRange(zscore(narrow ./ broad), [0 1]);
+%     zband = narrow ./ broad;
 else
-    zband = pband;
+    zband = narrow;
+end
+if ~isempty(smf)
+    zband = movmean(zband, smf);
+    pband = movmean(pband, smf);
 end
 zband = zband(:);
+
+% % smooth and z-score
+% if ~isempty(band)
+%     zband = sum(pband, 1);
+%     zband = movmean(zband, smf);
+%     if znorm
+%         zband = bz_NormToRange(zscore(zband), [0 1]);
+%     end
+% else
+%     zband = pband;
+% end
+% zband = zband(:);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % graphics
@@ -88,7 +105,7 @@ zband = zband(:);
 
 % ~same as Matlab default plot spectrogram
 if graphics    
-    surf(tband / 60, f, pband, 'EdgeColor', 'none');
+    surf(t / 60, f, pband, 'EdgeColor', 'none');
     axis xy;
     axis tight;
     view(0,90);
