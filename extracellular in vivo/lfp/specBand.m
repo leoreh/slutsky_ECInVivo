e5a4919883627461701dc5fb27545ed43a3326bd
leoreh <45538676+leoreh@users.noreply.github.com>
2020-06-14 07:18:31 +0300

    figs9

--- extracellular in vivo/lfp/specBand.m
+++ extracellular in vivo/lfp/specBand.m
@@ -3,15 +3,16 @@ function [zband, t] = specBand(varargin)
 % calculates power spectra in specified band. can be normalized to power in
 % broadband (0-100 Hz). if band is empty then simply calculates and plots
 % the broadband spectrogram.
-% 
+%
 % INPUT
 %   sig         signal for detection
 %   fs          sampling frequency {1250}.
-%   band        vector. e.g. {[1 4]} for delta 
+%   band        vector. e.g. {[1 4]} for delta
 %   binsize     scalar {10}. in [samples]
 %   smf         smooth factor {winsize}. empty means no smoothing. zero
 %               means smf = binsize
 %   normband    logical. normalize to broadband (1) or not {0}
+%   logaxis     logical. ploy y axis (f) on logscale {false}
 %   graphics    logical. plot figure {1}
 %
 % OUTPUT
@@ -22,7 +23,7 @@ function [zband, t] = specBand(varargin)
 %       # make compatible with band as matrix (multiple bands)
 %
 % CALLS
-%       spectrogram     
+%       spectrogram
 %
 % 13 jan 20 LH.     updates:
 % 20 feb 20 LH      normalize to broadband
@@ -31,23 +32,25 @@ function [zband, t] = specBand(varargin)
 % arguments
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-pband = inputParser;
-addParameter(pband, 'sig', [], @isnumeric)
-addParameter(pband, 'fs', 1250, @isnumeric)
-addParameter(pband, 'band', [1 4], @isnumeric)
-addParameter(pband, 'binsize', 10, @isnumeric)
-addParameter(pband, 'smf', 0, @isnumeric)
-addParameter(pband, 'normband', true, @islogical)
-addParameter(pband, 'graphics', false, @islogical)
+p = inputParser;
+addParameter(p, 'sig', [], @isnumeric)
+addParameter(p, 'fs', 1250, @isnumeric)
+addParameter(p, 'band', [1 4], @isnumeric)
+addParameter(p, 'binsize', 10, @isnumeric)
+addParameter(p, 'smf', 0, @isnumeric)
+addParameter(p, 'normband', true, @islogical)
+addParameter(p, 'logaxis', false, @islogical)
+addParameter(p, 'graphics', false, @islogical)
 
-parse(pband, varargin{:})
-sig = pband.Results.sig;
-fs = pband.Results.fs;
-band = pband.Results.band;
-binsize = pband.Results.binsize;
-smf = pband.Results.smf;
-normband = pband.Results.normband;
-graphics = pband.Results.graphics;
+parse(p, varargin{:})
+sig = p.Results.sig;
+fs = p.Results.fs;
+band = p.Results.band;
+binsize = p.Results.binsize;
+smf = p.Results.smf;
+normband = p.Results.normband;
+logaxis = p.Results.logaxis;
+graphics = p.Results.graphics;
 
 % params
 freq = logspace(0, 2, 100);
@@ -78,10 +81,12 @@ broad = sum(pband, 1);
 narrow = sum(pband(idx(1) : idx(2), :), 1);
 if normband
     zband = bz_NormToRange(zscore(narrow ./ broad), [0 1]);
-%     zband = narrow ./ broad;
+    %     zband = narrow ./ broad;
 else
     zband = narrow;
 end
+
+% smooth
 if ~isempty(smf)
     zband = movmean(zband, smf);
     pband = movmean(pband, smf);
@@ -105,7 +110,8 @@ zband = zband(:);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % ~same as Matlab default plot spectrogram
-if graphics    
+if graphics
+    pband = bz_NormToRange(pband, [0 1]);
     surf(t / 60, f, pband, 'EdgeColor', 'none');
     axis xy;
     axis tight;
@@ -116,12 +122,13 @@ if graphics
     ylabel('Frequency [Hz]');
     set(gca, 'Position', origSize);
     set(gca, 'TickLength', [0 0])
-    if isempty(band)
+    if logaxis
         set(gca, 'YScale', 'log')
     end
     box off
     title('Wideband spectrogram')
 end
+
 end
 
 % EOF
