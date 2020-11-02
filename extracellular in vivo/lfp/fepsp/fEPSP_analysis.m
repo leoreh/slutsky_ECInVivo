c19119d1b7bdb2c65e967a6090166e1a613f0933
Liordemarcas <58705788+Liordemarcas@users.noreply.github.com>
2020-11-02 11:44:38 +0200

    Shiri peak analysis, first attempt
    Comment the range base analysis
    Add a peak based analysis - 1st local maxima to 1st local minima
    Implant marks on waveAvg of mesuring points in graphics
    
    Mesurement behave incorrectly when signal is inversed - need to think
        of good auto inversing (corrent attempt doesnt give wanted results)

--- extracellular in vivo/lfp/fepsp/fEPSP_analysis.m
+++ extracellular in vivo/lfp/fepsp/fEPSP_analysis.m
@@ -139,11 +139,48 @@ for j = 1 : nspkgrp
         fepsp.traceAvg(j, i, :) = mean(fepsp.traces{j, i}, 2);
         switch protocol
             case 'io'
+%                 Generated bad results
+%                 if abs(min(fepsp.traces{j, i}(:))) > max(fepsp.traces{j, i}(:))
+%                     % I took this method from getLFP line 178, however
+%                     % changed the direction as it seemed weird the
+%                     % opposite. However, the syncronus/ asyncronus/
+%                     % inhibitory responce might affect this, and maybe with
+%                     % fepsp in PYR layer we would expect opposite direction.
+%                     % this will affect calculation because of the diff sensetivity of
+%                     % 1st local max and 1st local min, so we need to invert.
+%                     fprintf('Inverting traces in cell (%d,%d)\n',j,i)
+%                     fepsp.traces{j, i} = -fepsp.traces{j, i};
+%                 end
                 fepsp.waves{j, i} = fepsp.traces{j, i}(wvwin(1) : wvwin(2), :);
+                % find first local minima & maxima in window. since we
+                % expect the minima to be relativly big, ignore any minima
+                % with small Prominence (<0.3). if the responce is smaller
+                % then that, it is practicly 0.
+                [ValueCheckMax,PerTrace1Max] = max(islocalmax(fepsp.waves{j, i},1),[],1);
+                [ValueCheckMin,PerTrace1Min] = max(islocalmin(fepsp.waves{j, i},1,'MinProminence',0.3),[],1);
+                PerTrace1Max(ValueCheckMax == 0) = 1; 
+                %if no local maxima with any Prominence, take the 1st sample in window
+                PerTrace1Min(ValueCheckMin == 0) = round((fs/1000)*10-2*(fs/1000));
+                %if no local minima with minimal Prominence, take the sample corresponding to 10ms (expected responce pos)
+                PerTrace1MinIND = sub2ind(size(fepsp.waves{j, i}),PerTrace1Min,1:size(fepsp.waves{j, i},2));
+                PerTrace1MaxIND = sub2ind(size(fepsp.waves{j, i}),PerTrace1Max,1:size(fepsp.waves{j, i},2));
+                fepsp.ampcell{j, i} = abs(fepsp.waves{j, i}(PerTrace1MaxIND) - fepsp.waves{j, i}(PerTrace1MinIND));
                 fepsp.wavesAvg(j, i, :) = mean(fepsp.waves{j, i}, 2);
-                fepsp.ampcell{j, i} = range(fepsp.waves{j, i});
-                fepsp.amp(j, i) = mean(fepsp.ampcell{j, i});
-                                              
+                %fepsp.ampcell{j, i} = range(fepsp.waves{j, i});
+                %fepsp.amp(j, i) = mean(fepsp.ampcell{j, i});
+                
+                %Redo on mean wave
+                TheMin{j,i} = find(islocalmin(squeeze(fepsp.wavesAvg(j, i, :)),'MinProminence',0.3),1);
+                TheMax{j,i} = find(islocalmax(squeeze(fepsp.wavesAvg(j, i, :))),1);
+                if isempty(TheMin{j,i})
+                    TheMin{j,i} = round((fs/1000)*10-2*(fs/1000)); 
+                    %if no local minima with minimal Prominence, take the sample corresponding to 10ms (expected responce pos)
+                end
+                if isempty(TheMax{j,i})
+                    TheMax{j,i} = 1; %if no local maxima with any Prominence, take the 1st sample in window
+                end
+                fepsp.amp(j, i) = abs(fepsp.wavesAvg(j, i, TheMax{j,i}) - fepsp.wavesAvg(j, i, TheMin{j,i}));
+                
             case 'stp'
                 % note; after reviewing the data it seems that specifically
                 % for stp maximum absolute value may be better than range
@@ -179,17 +216,23 @@ if graphics
                 suptitle(sprintf('T#%d', i))
                 subplot(1, 2, 1)
                 plot(fepsp.tstamps(wvwin(1) : wvwin(2)), squeeze(fepsp.wavesAvg(i, :, :))')
+                hold on
+                SmallTSwindow = fepsp.tstamps(wvwin(1) : wvwin(2));
+                wavesAvgPos = sub2ind(size(fepsp.wavesAvg),ones(1,length([TheMin{i,:} TheMax{i,:}]))*i,repmat(1:size(fepsp.wavesAvg,2),1,2),[TheMin{i,:} TheMax{i,:}]);
+                plot(SmallTSwindow([TheMin{i,:}]),squeeze(fepsp.wavesAvg(wavesAvgPos(1:(end/2)))'),'*')
+                plot(SmallTSwindow([TheMax{i,:}]),squeeze(fepsp.wavesAvg(wavesAvgPos((end/2+1):end))'),'O')
                 axis tight
                 yLimit = [min([fepsp.wavesAvg(:)]) max([fepsp.wavesAvg(:)])];
                 ylim(yLimit)
                 xlabel('Time [ms]')
                 ylabel('Voltage [mV]')
-                legend(split(num2str(sort(fepsp.intens))))
+                legend([split(num2str(sort(fepsp.intens)));{'Amp measure point 1_s_t min'};{'Amp measure point 1_s_t max'}])
                 box off
                 
                 subplot(1, 2, 2)
-                ampmat = cell2nanmat(fepsp.ampcell(i, :));
-                boxplot(ampmat, 'PlotStyle', 'traditional')
+                %ampmat = cell2nanmat(fepsp.ampcell(i, :));
+                %boxplot(ampmat, 'PlotStyle', 'traditional')
+                bar(fepsp.amp(i,:))
                 ylim([min(horzcat(fepsp.ampcell{:})) max(horzcat(fepsp.ampcell{:}))])
                 xticklabels(split(num2str(sort(fepsp.intens))))
                 xlabel('Intensity [uA]')
